5dc4ac85f77cc2047cd7a8d59d7d1b8ea99587f3
taurenmage <burneressay@gmail.com>
2024-10-07 09:24:29 -0400

    giphy api project

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+API_KEY = "Sx91ZEFvgP4O5AwnGu6x2XgUam9GcB6q"
+ENDPOINT = "https://api.giphy.com/v1/gifs/trending"
+
+params = {"api_key": API_KEY, "limit": 3, "rating": "g"}
+response = requests.get(ENDPOINT, params=params)
+
+
+#status_code = response.status_code
+#print(status_code)
+
+response = response.json()
+
+for gif in response["data"]:
+
+    title = gif["title"]
+    trending_date = gif["trending_datetime"]
+    url = gif["url"]
+
+    print(f"{title} | {trending_date} | {url} ")
+
+
